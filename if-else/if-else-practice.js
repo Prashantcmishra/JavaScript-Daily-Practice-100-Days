179e9bc4026df9bc0b7936a63e80808b9e76b254
Prashant Mishra <Prashantcmishra@gmail.com>
2026-03-13 15:54:43 +0530

    Solve 5 questions of if-else

---
+++ if-else/if-else-practice.js
@@ -0,0 +1,49 @@
+// Q.1 Check if number is positive, negative or zero
+
+// let num  = 5;
+// if(num > 0){
+//     console.log("Positive Number")
+// }else if (num < 0){
+//     console.log("Negative number");
+// }else{
+//     console.log("Zero")
+// }
+
+
+// Q.2 Check if number is even or odd
+
+// let num = 3;
+// if(num % 2 === 0){
+//     console.log("Even number")
+// }else{
+//     console.log("Odd number")
+// }
+
+
+// Q.3 Check if number is zero
+
+// let num = -5;
+// if(num === 0){
+//     console.log("The number is zero")
+// }else{
+//     console.log("The number is not zero")
+// }
+
+
+// Q.4 Largest of two numbers
+
+// let a = 10 , b = 30;
+// if(a>b){
+//     console.log("a is the largest number");
+// }else{
+//     console.log("b is the largest number");
+// }
+
+// Q.5 Smallest of two numbers
+let a = 5 , b = 1;
+
+if(a<b){
+    console.log("a is the smallest number");
+}else{
+    console.log("b is the smallest number");
+}
