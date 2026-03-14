b5ba8bc7d291ff561b85c5aebb48affc78847493
Prashant Mishra <Prashantcmishra@gmail.com>
2026-03-14 18:17:43 +0530

    Solve 10 more questions on if-else

--- if-else/if-else-practice.js
+++ if-else/if-else-practice.js
@@ -149,16 +149,242 @@
 
 // Q.15 Assign grade
 
-let grade = 71;
+// let grade = 71;
 
-if(grade >=90){
-    console.log("A+ Grade")
-}else if (grade >= 75){
-    console.log("A Grade")
-}else if (grade >= 60){
-    console.log(" B Grade")
-}else if (grade >=40) {
-    console.log("C Grade")
-}else{
-    console.log("Fail")
-}
+// if(grade >=90){
+//     console.log("A+ Grade")
+// }else if (grade >= 75){
+//     console.log("A Grade")
+// }else if (grade >= 60){
+//     console.log(" B Grade")
+// }else if (grade >=40) {
+//     console.log("C Grade")
+// }else{
+//     console.log("Fail")
+// }
+
+
+// Q.16 Pass or Fail
+
+// let marks = 35;
+
+// if(marks >= 33){
+//     console.log("Pass");
+// }else{
+//     console.log("Fail")
+// }
+
+// Q.17 Prime number (basic)
+
+// let num = 7;
+
+// if(num > 1 && num % 2 !== 0 && num % 3 !== 0){
+//     console.log("Prime number basic check")
+// }else{
+//     console.log("Not prime");
+// }
+
+
+// Q.18  Temperature condition
+
+// let temp = 30;
+
+// if(temp >=35 ){
+//     console.log("Hot")
+// }else if (temp >=20){
+//     console.log("Normal")
+// }else{
+//     console.log("Cold")
+// }
+
+// Q.19 Salary taxable
+
+// let Salary = 50000;
+
+// if(Salary >= 45000){
+//     console.log("Taxable");
+// }else{
+//     console.timeLog("Not taxable");
+// }
+
+
+// Q.20 Login System 
+
+// let username = "admin";
+// let password = "1234";
+
+// if( username === 'admin' && password === '1234'){
+//     console.log("Login Successfull");
+// }else{
+//     console.log("Login failed")
+// }
+
+
+// Q.21 Alphabet check
+
+// let ch = 'A';
+
+// if((ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z")){
+//     console.log("Alphabet")
+// }else{
+//     console.log("Invalid Chracter")
+// }
+
+
+// Q.22 Uppercase or lowercase
+
+// let ch = 'A';
+
+// if(ch >= 'A' && ch <= 'Z'){
+//     console.log("UpperCase")
+// }else{
+//     console.log("LowerCase")
+// }
+
+
+// Q.23 Divisible by 7
+
+// let num = 14;
+// if(num % 7 === 0){
+//     console.log("Divisible by 7")
+// }else {
+//     console.log("Not Divisible by  7")
+// }
+
+
+// Q.24 Multiple of 3 and 7
+
+// let num = 21;
+
+// if(num % 3 === 0 && num % 7 === 0){
+//     console.log("Divisible by 7");
+// }else{
+//     console.log("Not Divisible by 7");
+// }
+
+
+// Q.25 Driving eligibility
+
+// let age = 16;
+
+// if(age >= 18){
+//     console.log("Eligibile for Driving")
+// }else{
+//     console.log("Not eligible for Driving")
+// }
+
+
+// Q.26 Movie ticket eligibility
+
+// let age = 12;
+
+// if(age >= 12){
+//     console.log("Eligible for Movie")
+// }else{
+//     console.log("Not eligible for Movie")
+// }
+
+
+// Q.27 Triangle validity
+
+// let a = 60 , b = 60 , c = 60;
+
+// if(a+b+c === 180){
+//     console.log("Valid Traingle")
+// }else{
+//     console.log("Invalid Triangle")
+// }
+
+
+// Q.28 Triangle type
+
+// let a = 5 , b = 5 , c = 5;
+
+// if(a === b && b === c){
+//     console.log("Equilateral Triangle")
+// }else if (a === b || b === c || a === c){
+//     console.log("Isosceles Triangle")
+// }else{
+//     console.log("Scalene Triangle")
+// }
+
+
+// Q.29 Largest of four numbers
+
+// let a = 5 , b = 7 , c = 10 , d = 20 ;
+
+// let max = a;
+
+// if( b > max) max = b;
+// if( c > max) max = c;
+// if( d > max) max = d;
+
+// console.log(max);
+
+
+// Q.30 Digit check
+
+// let ch = '5';
+
+// if (ch >=0 && ch <= 9){
+//     console.log("Digit")
+// }else{
+//     console.log("Not a Digit")
+// }
+
+
+// Q.31 Traffic light
+
+// let light = 'red';
+
+// if(light === "red") console.log("stop");
+// else if (light === "yellow") console.log("Wait");
+// else console.log("Go");
+
+
+// Q.32 Weekday or weekend
+
+// let day = 6;
+
+// if (day <= 5){
+//     console.log("Weekday")
+// }else{
+//     console.log("Weekend")
+// }
+
+
+// Q.33 Tax bracket
+
+// let income = 700000;
+
+// if (income < 250000) console.log("No Tax");
+// else if (income < 500000) console.log("5% Tax")
+// else console.log("20% Tax");
+
+
+// Q.34 Electricity bill
+
+// let unit = 119 ;
+
+// if (unit <= 100) console.log("Low Bill");
+// else console.log("High Bill")
+
+// Q.35 ATM withdrawal
+
+// let amount = 2000;
+
+// if(amount <= 10000) console.log("Allowed");
+// else console.log("Not Allowed");
+
+
+// Q.36 Positive even / negative odd
+
+// let num = 13;
+
+// if(num > 0 && num % 2 === 0){
+//     console.log("Positive Even")
+// }else if (num < 0 && num % 2 !== 0 ){
+//     console.log("Negative Odd")
+// }else{
+//     console.log("Normal and Both Condition did not matched")
+// }
