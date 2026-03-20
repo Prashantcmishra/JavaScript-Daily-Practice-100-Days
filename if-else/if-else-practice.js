a496fb768bdd46a56ee101a571ed901a4d213737
Prashant Mishra <Prashantcmishra@gmail.com>
2026-03-20 10:15:28 +0530

    feat : Switch Statement done

--- if-else/if-else-practice.js
+++ if-else/if-else-practice.js
@@ -423,4 +423,4 @@
 
 // if( age <= 18) console.log("Child");
 // else if (age < 60) console.log("Adult");
-// else console.log('Senior');
+// else console.log('Senior');  
---
+++ if-else/switch.js
@@ -0,0 +1,108 @@
+// Q.1 Day name from number
+
+// let day = 3;
+
+// switch (day) {
+//     case 1 :
+//         console.log("Monday");
+//         break ;
+//     case 2 : 
+//     console.log("Tuesday");
+//     break ;
+//     case 3 : 
+//     console.log("Wednesday");
+//     break;
+//     case 4 :
+//         console.log("Thursday");
+//         break ;
+//         case 5 :
+//             console.log("Friday");
+//             break ;
+//             case 6 : 
+//             console.log("Saturday");
+//             break ;
+//              case 7  :
+//              console.log("Sunday");
+//              break ;
+//              default : 
+//              console.log("Invalid Day");
+
+// }
+
+
+// Q.2 Month name
+
+// let month = 2;
+
+// switch (month) {
+//     case 1 : 
+//     console.log("January");
+//     break;
+//     case 2 : 
+//     console.log("February");
+//     break ;
+//     case 3 : 
+//     console.log("March");
+//     break ;
+//     default : 
+//     console.log("Invalid Month");
+// }
+
+
+// Q.3 Simple calculator
+
+
+// let a = 10 , b = 20 , op = '+';
+
+// switch (op) {
+//     case '+' :
+//         console.log(a + b);
+//         break;
+//         case '-':
+//         console.log(a - b);
+//         break ;
+//         case '*' : 
+//         console.log(a * b);
+//         break ;
+//         case '/' : 
+//         console.log(a /b);
+//         break ;
+//         default : 
+//         console.log("Invalid Operator");
+// }
+
+// Q.4 Vowel or consonant
+
+// let ch = 'a';
+
+// switch (ch) {
+//     case 'a':
+//         case 'e':
+//         case 'i' :
+//             case 'o':
+//                 case 'u':
+//                     console.log("Vowel");
+//                     break ;
+//                     default : 
+//                     console.log("Consonant");
+//                     // break ;
+// }
+
+
+// Q.5 Grade system
+
+let marks = 75;
+
+switch (true){
+    case (marks >= 90):
+        console.log("A");
+        break;
+        case (marks >= 60 ) :
+            console.log("B");
+            break ;
+            case (marks >= 40) :
+                console.log("C");
+                break ;
+                default :
+                console.log("Fail");
+}
