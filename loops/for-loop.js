dd8d9c883505106ddbd7734ac464d2d0580fbcca
Prashant Mishra <Prashantcmishra@gmail.com>
2026-03-27 12:59:12 +0530

    feat : more questions on loops

---
+++ loops/for-loop.js
@@ -0,0 +1,37 @@
+// Q.1 Print 1 to 10 
+
+// for(let i = 1; i<= 10 ; i++){
+//     console.log(i);
+// }
+
+
+// Q.2  Print 10 to 1
+
+// for(let i = 10 ; i >=1 ; i--){
+//     console.log(i)
+// }
+
+// Q.3 Print 1 to N
+
+// let n = 5;
+// for(let i= 1 ; i <= n ; i++){
+//     console.log(i);
+// }
+
+// Q.4 Even numbers
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+// Q.5 Odd Numbers 
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     if(i % 2 !== 0){
+//      console.log(i)
+//     }
+// }
